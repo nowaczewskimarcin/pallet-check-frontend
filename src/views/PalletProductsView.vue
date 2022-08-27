2216549e9eb8656055372bda58bb71825ac13f3d
nowaczewskimarcin <nowaczewskimarcin@gmail.com>
2022-08-27 16:41:28 +0200

    add coretly name to product name

--- src/views/PalletProductsView.vue
+++ src/views/PalletProductsView.vue
@@ -1,7 +1,8 @@
 <template>
 
-    <v-card class="mx-auto" max-width="1000" :loading="loading">
-        <v-card-title>Paleta do sprawdzenia o numerze:</v-card-title>
+    <v-card class="mx-auto" max-width="1000">
+        <v-card-title :palletNumber="palletNumber">Produkty na palecie o numerze: {{ palletNumber }}
+        </v-card-title>
         <v-container class="mb-3 pa-md-5 mx-lg-auto">
 
             <v-simple-table>
@@ -22,7 +23,7 @@
                     <tbody>
                         <tr v-for="(product, index) in products" :key="product.index">
                             <td> {{ index + 1 }}</td>
-                            <td>{{ product.aaa }}</td>
+                            <td>{{ product.name }}</td>
                             <td> {{ product.amount }}</td>
                         </tr>
                     </tbody>
@@ -38,15 +39,18 @@
 export default {
     name: 'PalletProductsView',
     props: {
+        palletNumber: {
+            required: true,
+            type: String,
+        }
     },
     data() {
         return {
-            palletNumber: null,
             products: [
-                { aaa: "Słuchawki", size: "małe", amount: 0 }, { aaa: "Kierownica", size: "duże", amount: 0 },
-                { aaa: "Klawiatura", size: "duże", amount: 0 }, { aaa: "Joystick", size: "małe", amount: 0 },
-                { aaa: "Mysz", size: "małe", amount: 0 }, { aaa: "Podkładka pod mysz", size: "duże", amount: 0 },
-                { aaa: "Głośniki", size: "duże", amount: 0 }, { aaa: "Kabel HDMI", size: "duże", amount: 0 },
+                { name: "Słuchawki", size: "małe", amount: 0 }, { name: "Kierownica", size: "duże", amount: 0 },
+                { name: "Klawiatura", size: "duże", amount: 0 }, { name: "Joystick", size: "małe", amount: 0 },
+                { name: "Mysz", size: "małe", amount: 0 }, { name: "Podkładka pod mysz", size: "duże", amount: 0 },
+                { name: "Głośniki", size: "duże", amount: 0 }, { name: "Kabel HDMI", size: "duże", amount: 0 },
             ],
 
         }
