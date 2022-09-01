27b0f3f7da2127599e5e186e0441fe537115a9a1
nowaczewskimarcin <nowaczewskimarcin@gmail.com>
2022-09-01 15:37:51 +0200

    add type="number" to text-fields

--- src/views/PalletProductsView.vue
+++ src/views/PalletProductsView.vue
@@ -23,7 +23,8 @@
                             <td> {{ index + 1 }}</td>
                             <td>{{ product.name }}</td>
                             <td>
-                                <v-text-field v-model="product.quantity" required placeholder="Wpisz ilość produktu">
+                                <v-text-field v-model="product.quantity" type="number" required
+                                    placeholder="Wpisz ilość produktu">
                                 </v-text-field>
                             </td>
                         </tr>
